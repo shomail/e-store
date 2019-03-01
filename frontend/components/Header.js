35b55595658c3360511d1c34a1cd3d1932621f88
Shomail <shomail1988@gmail.com>
2019-03-01 23:21:50 +0100

    base header styling

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -15,14 +15,33 @@ const Logo = styled.h1`
     text-transform: uppercase;
     text-decoration: none;
   }
-  @media (max-width: 1300px) {
+  @media (max-width: 1200px) {
     margin: 0%;
     text-align: center;
   }
 `;
 
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1200px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
+  }
+`;
+
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
       <Logo>
         <Link href="/">
@@ -35,7 +54,7 @@ const Header = () => (
       <p>Search</p>
     </div>
     <div>Cart</div>
-  </div>
+  </StyledHeader>
 );
 
 export default Header;
