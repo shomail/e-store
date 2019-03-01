00988e691deea0abcf69212f0431961670776ee1
Shomail <shomail1988@gmail.com>
2019-03-01 12:16:58 +0100

    base nextjs config and custom app comp added

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+const Home = (props) => (
+  <div>
+    <p>Welcome to Sick fits E-Store</p>
+    <Link href="/sell">Sell you Items</Link>
+  </div>
+)
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const Sell = (props) => (
+  <div>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+    <p>Sell your products easily with us.</p>
+
+  </div>
+)
+
+export default Sell;
