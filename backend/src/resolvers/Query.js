426c540b0f976f2a89a4a2c06e58b02bce1aad3a
Shomail <shomail1988@gmail.com>
2019-03-02 23:14:17 +0100

    write test graphql Query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,7 @@
-const Query = {};
+const Query = {
+    users(parent, args, ctx, info) {
+        return [{name: 'shomail', email: 'sh@mail.com'}, {name: 'shoby', email:'shoby@mail.com'}]
+    }
+};
 
 module.exports = Query;
