fd04b69d4db1a227cd6a590735c559bd93877d6b
Shomail <shomail1988@gmail.com>
2019-03-03 00:49:14 +0100

    Prettier Eslint integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,20 @@
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
-const db = require('./db');
+const db = require('./db')
 
 const server = createServer()
 
 //TODO use express middleware to handle cookies (JWT)
-//TODO use express middleware to populate current user 
+//TODO use express middleware to populate current user
 
-server.start({
+server.start(
+  {
     cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL
-    }
-}, deets => {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`)
-})
+  },
+)
