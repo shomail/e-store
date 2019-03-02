3984aea5ccaf4329e39d4c50286a4043df59da13
Shomail <shomail1988@gmail.com>
2019-03-02 22:54:39 +0100

    graphql yoga backend setup and config

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+//Connect to remote prisma db and enable us to query it with JS
+const {Prisma} = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+});
+
+module.exports = db
