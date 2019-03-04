32b2b256b8fad3c7de501a2bf7f29e96b5b240df
Shomail <shomail1988@gmail.com>
2019-03-04 17:12:16 +0100

    edit item mutation and query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -12,6 +12,17 @@ const Mutations = {
 
     return item
   },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args }
+    delete updates.id
+    return ctx.db.mutation.updateItem(
+      {
+        data: { updates },
+        where: { id: args.id },
+      },
+      info,
+    )
+  },
 }
 
 module.exports = Mutations
