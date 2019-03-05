a5fb4ccaf6c442aaaf0542496cb6a7977a22116f
Shomail <shomail1988@gmail.com>
2019-03-05 13:50:49 +0100

    pagination styling and querries

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,10 +3,7 @@ const { forwardTo } = require('prisma-binding')
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
-  //   async items(parent, args, ctx, info) {
-  //     const items = await ctx.db.query.items()
-  //     return items
-  //   },
+  itemsConnection: forwardTo('db'),
 }
 
 module.exports = Query
