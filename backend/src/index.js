9710b6c6cf9366667c1b9b2f9f7bc3b267fb054c
Shomail <shomail1988@gmail.com>
2019-03-06 15:47:36 +0100

    JWT parsing in request middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
 const db = require('./db')
@@ -6,6 +7,17 @@ const db = require('./db')
 const server = createServer()
 
 server.express.use(cookieParser())
+
+//decode the JWT to get userId on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    //put userId on req for further req to access
+    req.userId = userId
+  }
+  next()
+})
 //TODO use express middleware to populate current user
 
 server.start(
