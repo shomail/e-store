48c92478c415f49b90fc16bd6b3ea880942b78fb
Shomail <shomail1988@gmail.com>
2019-03-14 21:11:56 +0100

    add animation to cart count change

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import styled from 'styled-components';
+
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    transition: all 0.4s;
+    backface-visibility: hidden;
+  }
+  .count-enter {
+    transform: scale(4) rotateX(0.5turn);
+  }
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+  .count-exit-active {
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
 
 const Dot = styled.div`
   background: ${props => props.theme.red};
@@ -15,7 +40,21 @@ const Dot = styled.div`
   font-variant-numeric: tabular-nums;
 `;
 
-const CartCount = ({ count }) => <Dot>{count}</Dot>;
+const CartCount = ({ count }) => (
+  <AnimationStyles>
+    <TransitionGroup>
+      <CSSTransition
+        unmountOnExit
+        className="count"
+        classNames="count"
+        key={count}
+        timeout={{ enter: 400, exit: 400 }}
+      >
+        <Dot>{count}</Dot>
+      </CSSTransition>
+    </TransitionGroup>
+  </AnimationStyles>
+);
 
 CartCount.propTypes = {
   count: PropTypes.number.isRequired,
