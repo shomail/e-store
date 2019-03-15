cbfcfecd876f919082c3cfd4ae94be2a88021790
Shomail <shomail1988@gmail.com>
2019-03-15 14:57:53 +0100

    Search Query added

--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -6,12 +6,47 @@ import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
 import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
 
+const SEARCH_ITEM_QUERY = gql`
+  query SEARCH_ITEM_QUERY($searchTerm: String!) {
+    items(
+      where: {
+        OR: [
+          { title_contains: $searchTerm }
+          { description_contains: $searchTerm }
+        ]
+      }
+    ) {
+      id
+      image
+      title
+    }
+  }
+`;
+
 class AutoComplete extends Component {
+  onChange = async (e, client) => {
+    const res = await client.query({
+      query: SEARCH_ITEM_QUERY,
+      variables: { searchTerm: e.target.value },
+    });
+    console.log(res);
+  };
+
   render() {
     return (
       <SearchStyles>
         <div>
-          <input type="search" />
+          <ApolloConsumer>
+            {client => (
+              <input
+                type="search"
+                onChange={e => {
+                  e.persist();
+                  this.onChange(e, client);
+                }}
+              />
+            )}
+          </ApolloConsumer>
           <DropDown>
             <p>Items will go here</p>
           </DropDown>
