2e6077e41e1b865024fe799db55cb2c8cf2f82ea
Shomail <shomail1988@gmail.com>
2019-03-17 12:52:30 +0100

    stripe checkout component added

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -9,9 +9,29 @@ import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
+function totalItems(cart) {
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+}
+
 class TakeMyMoney extends Component {
   render() {
-    return <User>{({ data: { me } }) => <p>{this.props.children}</p>}</User>;
+    return (
+      <User>
+        {({ data: { me } }) => (
+          <StripeCheckout
+            amount={calcTotalPrice(me.cart)}
+            name="E-Store"
+            description={`Order of ${totalItems(me.cart)} items!`}
+            image={me.cart[0].item && me.cart[0].item.image}
+            stripeKey="pk_test_SfT2BaSIxDhmbYk4kOnBBzGT"
+            currency="USD"
+            email={me.email}
+          >
+            {this.props.children}
+          </StripeCheckout>
+        )}
+      </User>
+    );
   }
 }
 
