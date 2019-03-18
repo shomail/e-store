7159b5dd6eb019098a23b5114b9a1fbedf5f349b
Shomail <shomail1988@gmail.com>
2019-03-18 21:20:33 +0100

    ordersList template done

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -6,16 +6,80 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import formatMoney from '../lib/formatMoney';
 import OrderItemStyles from './styles/OrderItemStyles';
+import ErrorMessage from './ErrorMessage';
 
-// const QUERY_ORDER_LIST = {
-//     query orderList {
+const USER_ORDERS_QUERY = gql`
+  query USER_ORDERS_QUERY {
+    orders(orderBy: createdAt_DESC) {
+      id
+      total
+      createdAt
+      items {
+        id
+        title
+        price
+        description
+        quantity
+        image
+      }
+    }
+  }
+`;
 
-//     }
-// }
+const OrderUl = styled.ul`
+  display: grid;
+  grid-gap: 4rem;
+  grid-template-columns: repeat(auto-fit, minmax(40%, 1fr));
+`;
 
 class OrderList extends Component {
   render() {
-    return <p>Orders List</p>;
+    return (
+      <Query query={USER_ORDERS_QUERY}>
+        {({ data: { orders }, error, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <ErrorMessage error={error} />;
+          return (
+            <div>
+              <h2>You have {orders.length} orders</h2>
+              <OrderUl>
+                {orders.map(order => (
+                  <OrderItemStyles key={order.id}>
+                    <Link
+                      href={{ pathname: '/order', query: { id: order.id } }}
+                    >
+                      <a>
+                        <div className="order-meta">
+                          <p>
+                            {order.items.reduce(
+                              (tally, item) => tally + item.quantity,
+                              0
+                            )}{' '}
+                            Items
+                          </p>
+                          <p>{order.items.length} Products</p>
+                          <p>{formatDistance(order.createdAt, new Date())}</p>
+                          <p>{formatMoney(order.total)}</p>
+                        </div>
+                        <div className="images">
+                          {order.items.map(item => (
+                            <img
+                              key={item.id}
+                              src={item.image}
+                              alt={item.title}
+                            />
+                          ))}
+                        </div>
+                      </a>
+                    </Link>
+                  </OrderItemStyles>
+                ))}
+              </OrderUl>
+            </div>
+          );
+        }}
+      </Query>
+    );
   }
 }
 
