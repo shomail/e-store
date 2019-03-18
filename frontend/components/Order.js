dc2cb1f42886bc0015955d2566c2eabeb4336541
Shomail <shomail1988@gmail.com>
2019-03-18 15:36:34 +0100

    updated datamodel and order query added

--- frontend/components/Order.js
+++ frontend/components/Order.js
@@ -8,6 +8,28 @@ import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
 import OrderStyles from './styles/OrderStyles';
 
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order(id: $id) {
+      id
+      charge
+      total
+      createdAt
+      user {
+        id
+      }
+      items {
+        id
+        title
+        description
+        price
+        image
+        quantity
+      }
+    }
+  }
+`;
+
 class Order extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
@@ -16,9 +38,18 @@ class Order extends Component {
   render() {
     const { id } = this.props;
     return (
-      <div>
-        <p>Order Id: {id}</p>
-      </div>
+      <Query query={SINGLE_ORDER_QUERY} variables={{ id }}>
+        {({ data, error, loading }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          console.log(data);
+          return (
+            <div>
+              <p>Order Id: {id}</p>
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
