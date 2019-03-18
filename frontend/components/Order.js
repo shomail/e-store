98d513d21fe2c8eaca221f08ed4e02ff3ab7b090
Shomail <shomail1988@gmail.com>
2019-03-18 15:55:20 +0100

    Order component template done

--- frontend/components/Order.js
+++ frontend/components/Order.js
@@ -42,11 +42,47 @@ class Order extends Component {
         {({ data, error, loading }) => {
           if (error) return <Error error={error} />;
           if (loading) return <p>Loading...</p>;
-          console.log(data);
+          const { order } = data;
           return (
-            <div>
-              <p>Order Id: {id}</p>
-            </div>
+            <OrderStyles>
+              <Head>
+                <title>Sick Fits - Order {order.id}</title>
+              </Head>
+              <p>
+                <span>Order ID:</span>
+                <span>{id}</span>
+              </p>
+              <p>
+                <span>Charge</span>
+                <span>{order.charge}</span>
+              </p>
+              <p>
+                <span>Date</span>
+                <span>{format(order.createdAt, 'MMMM, d, YYYY h:mm a')}</span>
+              </p>
+              <p>
+                <span>Total</span>
+                <span>{formatMoney(order.total)}</span>
+              </p>
+              <p>
+                <span>Item Count</span>
+                <span>{order.items.length}</span>
+              </p>
+              <div className="items">
+                {order.items.map(item => (
+                  <div className="order-item" key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <div className="item-details">
+                      <h2>{item.title}</h2>
+                      <p>Qty: {item.quantity}</p>
+                      <p>Each: {formatMoney(item.price)}</p>
+                      <p>SubTotal: {formatMoney(item.price * item.quantity)}</p>
+                      <p>{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </OrderStyles>
           );
         }}
       </Query>
