0b759cf639ad656f403b0a74aa0dbec9b116a2db
Shomail <shomail1988@gmail.com>
2019-03-19 22:44:38 +0100

    whole and fractional amount test added

--- frontend/__tests__/formatMoney.test.js
+++ frontend/__tests__/formatMoney.test.js
@@ -11,4 +11,10 @@ describe('formatMoney Function', () => {
     expect(formatMoney(100)).toEqual('$1');
     expect(formatMoney(1000000)).toEqual('$10,000');
   });
+
+  it('works with whole and fractional dollars', () => {
+    expect(formatMoney(5012)).toEqual('$50.12');
+    expect(formatMoney(101)).toEqual('$1.01');
+    expect(formatMoney(110)).toEqual('$1.10');
+  });
 });
