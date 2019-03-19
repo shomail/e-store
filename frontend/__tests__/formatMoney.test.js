b8cd9185113f3ed15910ffaf486ff919a2dfccbb
Shomail <shomail1988@gmail.com>
2019-03-19 22:41:05 +0100

    whole dollar test added

--- frontend/__tests__/formatMoney.test.js
+++ frontend/__tests__/formatMoney.test.js
@@ -5,4 +5,10 @@ describe('formatMoney Function', () => {
     expect(formatMoney(1)).toEqual('$0.01');
     expect(formatMoney(10)).toEqual('$0.10');
   });
+
+  it('leaves cents of for whole dollars', () => {
+    expect(formatMoney(5000)).toEqual('$50');
+    expect(formatMoney(100)).toEqual('$1');
+    expect(formatMoney(1000000)).toEqual('$10,000');
+  });
 });
