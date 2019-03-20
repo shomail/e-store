e39780b2856a07d3357e8ac27e24de21bcf8e771
Shomail <shomail1988@gmail.com>
2019-03-20 14:43:48 +0100

    modified ItemComponent test

---
+++ frontend/__tests__/Item.test.js
@@ -0,0 +1,34 @@
+import { shallow } from 'enzyme';
+import ItemComponent from '../components/Item';
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A cool item',
+  price: 5000,
+  description: 'This Item is really cool!',
+  image: 'dog.jpg',
+  largeImage: 'largedog.jpg',
+};
+
+describe('<Item />', () => {
+  it('renders the image properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const img = wrapper.find('img');
+    expect(img.props().src).toBe(fakeItem.image);
+    expect(img.props().alt).toBe(fakeItem.title);
+  });
+  it('renders the pricetag and title', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const PriceTag = wrapper.find('PriceTag');
+    expect(PriceTag.children().text()).toBe('$50');
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+  });
+  it('renders the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const buttonList = wrapper.find('.buttonList');
+    expect(buttonList.children()).toHaveLength(3);
+    expect(buttonList.find('Link').exists()).toBe(true);
+    expect(buttonList.find('AddToCart').exists()).toBe(true);
+    expect(buttonList.find('DeleteItem').exists()).toBe(true);
+  });
+});
--- frontend/__tests__/sample.test.js
+++
@@ -1,5 +0,0 @@
-describe('sample test', () => {
-  it('works as expected', () => {
-    expect(1).toEqual(1);
-  });
-});
