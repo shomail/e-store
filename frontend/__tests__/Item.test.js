a882a2b067bb422c644a656e8b4ee0d2f241e20a
Shomail <shomail1988@gmail.com>
2019-03-20 14:57:23 +0100

    add ItemComponent snapshot

--- frontend/__tests__/Item.test.js
+++ frontend/__tests__/Item.test.js
@@ -1,16 +1,21 @@
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import ItemComponent from '../components/Item';
 
 const fakeItem = {
   id: 'ABC123',
   title: 'A cool item',
-  price: 5000,
+  price: 4000,
   description: 'This Item is really cool!',
   image: 'dog.jpg',
   largeImage: 'largedog.jpg',
 };
 
 describe('<Item />', () => {
+  it('renders and matches the snapshot', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
   it('renders the image properly', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem} />);
     const img = wrapper.find('img');
@@ -20,7 +25,7 @@ describe('<Item />', () => {
   it('renders the pricetag and title', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem} />);
     const PriceTag = wrapper.find('PriceTag');
-    expect(PriceTag.children().text()).toBe('$50');
+    expect(PriceTag.children().text()).toBe('$40');
     expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
   });
   it('renders the buttons properly', () => {
