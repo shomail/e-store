3b9028b3d0409b5fe632a8a57152942003a1e77e
Shomail <shomail1988@gmail.com>
2019-03-22 11:36:47 +0100

    guest user nav snapshot added

---
+++ frontend/__tests__/Nav.test.js
@@ -0,0 +1,36 @@
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { CURRENT_USER_QUERY } from '../components/User';
+import Nav from '../components/Nav';
+import { fakeUser } from '../lib/testUtils';
+
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } },
+  },
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } },
+  },
+];
+
+describe('<Nav />', () => {
+  it('renders guest user nav', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={notSignedInMocks}>
+        <Nav />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const nav = wrapper.find('[data-test="nav"]');
+    // console.log(nav.debug());
+    expect(toJSON(nav)).toMatchSnapshot();
+  });
+});
