c8d3466e9bb0aaa82929e306dad4621ff9f0ab56
Shomail <shomail1988@gmail.com>
2019-03-26 13:14:05 +0100

    add to cart button render and snapshot test

---
+++ frontend/__tests__/AddToCart.test.js
@@ -0,0 +1,32 @@
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { ApolloConsumer } from 'react-apollo';
+import AddToCart, { ADD_TO_CART_MUTATION } from '../components/AddToCart';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { fakeUser, fakeCartItem } from '../lib/testUtils';
+
+const mocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: { ...fakeUser(), cart: [] } } },
+  },
+  {
+    request: { query: ADD_TO_CART_MUTATION, variables: { id: 'abc123' } },
+    result: { data: { addToCart: { ...fakeCartItem(), quantity: 1 } } },
+  },
+];
+
+describe('<AddToCart />', () => {
+  it('', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <AddToCart id="abc123" />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(toJSON(wrapper.find('button'))).toMatchSnapshot();
+  });
+});
