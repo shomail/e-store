3aedb25bb3a2ea69afa57d800531455d92630ece
Shomail <shomail1988@gmail.com>
2019-03-26 12:52:12 +0100

    signup form test

--- frontend/__tests__/Signup.test.js
+++ frontend/__tests__/Signup.test.js
@@ -2,9 +2,16 @@ import { mount } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import wait from 'waait';
 import { MockedProvider } from 'react-apollo/test-utils';
+import { ApolloConsumer } from 'react-apollo';
 import Signup, { SIGNUP_MUTATION } from '../components/Signup';
 import { CURRENT_USER_QUERY } from '../components/User';
 import { fakeUser } from '../lib/testUtils';
+
+function type(wrapper, name, value) {
+  wrapper.find(`input[name="${name}"]`).simulate('change', {
+    target: { name, value },
+  });
+}
 
 const me = fakeUser();
 const mocks = [
@@ -44,5 +51,29 @@ describe('<Signup />', () => {
       </MockedProvider>
     );
     expect(toJSON(wrapper.find('form'))).toMatchSnapshot();
+  });
+  it('calls the mutation properly', async () => {
+    let apolloClient;
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client;
+            return <Signup />;
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    type(wrapper, 'name', me.name);
+    type(wrapper, 'email', me.email);
+    type(wrapper, 'password', 'me123');
+    wrapper.update();
+    wrapper.find('form').simulate('submit');
+    await wait();
+    // query the user from apolloClient
+    const user = await apolloClient.query({ query: CURRENT_USER_QUERY });
+    expect(user.data.me).toMatchObject(me);
   });
 });
